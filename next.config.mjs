d1c0ffccca0f6d81bd2be2ef1036b41376900a6d
Madhav Setia <madhavsetia24@gmail.com>
2024-04-12 15:14:33 +0530

    changed config temporarily

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["supabase.dirtychat.ai"],
+    domains: ["supabase.dirtychat.ai", "jsjoswfatcvghzacmiqj.supabase.co"],
   },
 };
 
